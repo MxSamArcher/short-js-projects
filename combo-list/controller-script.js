fe45caa8fe0fcd2bd53f3ec083d38bc303dc661d
Sam Archer <mx.sam.archer@gmail.com>
2024-02-11 07:35:07 +1100

    Created new directory for the combo-app and copied the list function creator. Also added a unique identifier to the class objects instantiated inside listName object

---
+++ combo-list/controller-script.js
@@ -0,0 +1,21 @@
+function createNewList () {
+  const listName = getTextBoxContents();
+  if (listName) {
+      lists[listName + '.' + new Date().getTime()] = new ListBinding(listName)
+      console.log(`${listName} has been added successfully!`);
+  } else {
+      console.log('Text field is empty')
+  }
+  document.getElementById('list-name-text-box').value = '';
+}
+
+function getTextBoxContents () {
+  const listNameTextBoxContents = document.getElementById('list-name-text-box').value;
+  return listNameTextBoxContents;
+}
+
+document.getElementById('list-name-text-box').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+      createNewList();
+  }
+})
